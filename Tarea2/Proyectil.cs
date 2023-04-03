aeecafe0b097167d1a533d05b019b7c5f7db8aaf
rodrigocormel <go.cormel@hotmail.com>
2023-04-03 12:52:27 -0500

    Proyectil

---
+++ Tarea2/Proyectil.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea2
+{
+    class Proyectil
+    {
+        public float daño;
+        public string nombre;
+
+        public Proyectil(float daño , string nombre) 
+        {
+            this.nombre = nombre;
+            this.daño = daño;
+        }
+
+        public string ObtenerNombre()
+        {
+            return nombre;
+        }
+
+        public float Daño()
+        {
+            return daño;
+        }
+    }
+}

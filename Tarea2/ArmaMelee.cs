8a22b959ecd7adc14ef4e5053ea297e81c59e276
jorgeluis15520 <103298795+jorgeluis15520@users.noreply.github.com>
2023-04-03 12:38:39 -0500

    Arma Melee

---
+++ Tarea2/ArmaMelee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea2
+{
+    internal class ArmaMelee : Arma
+    {
+        public ArmaMelee(string nombre, float daño, float velocidadAtaque,int precio)
+        {
+            this.nombre = nombre;
+            this.daño = daño;
+            this.velocidadAtaque = velocidadAtaque;
+            this.precio = precio;
+        }
+
+        public override float DañoPorSegundo()
+        {
+            return daño / velocidadAtaque;
+        }
+    }
+}
